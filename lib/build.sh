3a910e0f3fb9028cb5e0e95679edb2e231e85b88
Florian Schrag <florian@schrag.org>
2015-05-18 17:54:53 +0200

    install compass again

--- lib/build.sh
+++ lib/build.sh
@@ -132,6 +132,35 @@ install_node() {
   PATH=$heroku_dir/node/bin:$PATH
 }
 
+install_compass() {
+	# install compass
+	status "Installing Compass"
+	if [ "$STACK" == "cedar-14" ]; then
+	  export GEM_HOME=$cache_dir/ruby/.gem/ruby/2.2.0
+	else
+	  export GEM_HOME=$cache_dir/ruby/.gem/ruby/1.9.1
+	fi
+	
+	PATH="$GEM_HOME/bin:$PATH"
+	if test -d $cache_dir/ruby/.gem; then
+	  status "Restoring ruby gems directory from cache"
+	  cp -r $cache_dir/ruby/.gem $build_dir
+	  HOME=$build_dir gem update compass --user-install --no-rdoc --no-ri
+	else
+	  HOME=$build_dir gem install compass --user-install --no-rdoc --no-ri
+	fi
+	
+	# cache ruby gems compass
+	rm -rf $cache_dir/ruby
+	mkdir -p $cache_dir/ruby
+
+	# If app has a gems directory, cache it.
+	if test -d $build_dir/.gem; then
+	  status "Caching ruby gems directory for future builds"
+	  cp -r $build_dir/.gem $cache_dir/ruby
+	fi
+}
+
 install_iojs() {
   local iojs_engine=$1
 
